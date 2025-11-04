1e5b1e84d00b1e0563ee8456cdc9b68c6b1410a6
Shravani Gurav <shravanigurav049@gmail.com>
2025-11-05 01:32:32 +0530

    message

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Bricolage_Grotesque } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import NavItems from "@/components/NavItems";
 
 const bricolage = Bricolage_Grotesque({
     variable: "--font-bricolage",
@@ -20,6 +22,9 @@ export default function RootLayout({
     return (
         <html lang="en">
         <body className={`${bricolage.variable} antialiased`}>
+        <Navbar/>
+
+
         {children}
         </body>
         </html>
